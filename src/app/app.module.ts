bda23fc65febecd3ce81ac7298454cb5d17203d2
Elton Andrade <eltonsa75@hotmail.com>
2019-04-13 12:21:07 -0300

    Validação do formulário de Pedido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
 
 import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
 import { OrderComponent } from './order/order.component';
+import { InputComponent } from './shared/input/input.component';
 
 @NgModule({
   declarations: [
@@ -36,7 +37,8 @@ import { OrderComponent } from './order/order.component';
     MenuItemComponent,
     ShoppingCartComponent,
     ReviewsComponent,
-    OrderComponent
+    OrderComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule,
