93fdb678ec0d84d65c256a910bd71448cfc03a9a
Elton Andrade <eltonsa75@hotmail.com>
2019-04-21 15:16:37 -0300

    Formulário com Reactive Form / Validator

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -3,7 +3,7 @@ import { RadioOption } from './../shared/radio/radio-option.model';
 import { Component, OnInit } from '@angular/core';
 import { OrderService } from './order.service';
 
-import{FormGroup, FormBuilder} from '@angular/forms';
+import{FormGroup, FormBuilder, Validators, AbstractControl} from '@angular/forms';
 
 
 import {Router} from '@angular/router';
@@ -18,8 +18,11 @@ import{Order, OrderItem} from "./order.model";
 })
 export class OrderComponent implements OnInit {
 
-  orderForm: FormGroup
+  emailPattern  = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
 
+  numberPattern = /^[0-9]*$/
+
+  orderForm: FormGroup
 
   delivery: number = 8;
 
@@ -29,20 +32,33 @@ export class OrderComponent implements OnInit {
     {label: 'Cartão Refeição ', value: 'REF'},
   ];
 
-  constructor(private orderService: OrderService, 
+  constructor(private orderService: OrderService,
               private router: Router,
               private formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.orderForm = this.formBuilder.group({
-      name: '',
-      email: this.formBuilder.control(''),
-      emailConfirmation: this.formBuilder.control(''),
-      address: this.formBuilder.control(''),
-      number: this.formBuilder.control(''),
+      name: this.formBuilder.control('', [Validators.required, Validators.minLength(5)]),
+      email: this.formBuilder.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
+      emailConfirmation: this.formBuilder.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
+      address: this.formBuilder.control('', [Validators.required, Validators.minLength(5)]),
+      number: this.formBuilder.control('', [Validators.required, Validators.pattern(this.numberPattern)]),
       optionalAddress: this.formBuilder.control(''),
-      paymentOptions: this.formBuilder.control('')
-    })
+      paymentOption: this.formBuilder.control('', [Validators.required])
+    }, {validator: OrderComponent.equalsTo})
+  }
+
+  static equalsTo(group: AbstractControl): {[key:string]: boolean} {
+    const email = group.get('email')
+    const emailConfirmation = group.get('emailConfirmation')
+    if(!email || !emailConfirmation){
+      return undefined
+    }
+
+    if (email.value !== emailConfirmation.value){
+      return {emailsNotMatch:true}
+    }
+    return undefined
   }
 
 
--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, ContentChild, AfterContentInit } from '@angular/core';
-import {NgModel} from '@angular/forms';
+import {NgModel, FormControlName} from '@angular/forms';
 
 
 
@@ -16,6 +16,7 @@ export class InputComponent implements OnInit, AfterContentInit {
   input: any;
 
   @ContentChild(NgModel) model: NgModel;
+  @ContentChild(FormControlName) control: FormControlName;
 
   constructor() { }
 
@@ -23,9 +24,9 @@ export class InputComponent implements OnInit, AfterContentInit {
   }
 
   ngAfterContentInit() {
-   this.input = this.model;
+   this.input = this.model || this.control;
    if (this.input === undefined) {
-     throw new Error('Esse componente precisa ser usado com uma diretiva ngModel')
+     throw new Error('Esse componente precisa ser usado com uma diretiva ngModel ou formControlName')
    }
   }
 
