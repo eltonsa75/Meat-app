78c2a33d947a1a15982a2784caca60beb73d3f91
Elton Andrade <eltonsa75@hotmail.com>
2019-04-21 19:14:38 -0300

    Juntando o Core com Shared Serviços

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,27 @@
 import { FormsModule, ReactiveFormsModule, FormControlName } from '@angular/forms';
-import { NgModule } from '@angular/core';
-import{CommonModule} from '@angular/common';
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { RatingComponent } from './rating/rating.component';
 import { RadioComponent } from './radio/radio.component';
 import { InputComponent } from './input/input.component';
 
+import { ShoppingCartService } from 'app/restaurant-detail/shopping-cart/shopping-cart.service';
+import { RestaurantsServices } from 'app/restaurants/restaurants.services';
+import { OrderService } from 'app/order/order.service';
+
 @NgModule({
     declarations: [InputComponent, RadioComponent, RatingComponent],
     imports: [CommonModule, FormsModule, ReactiveFormsModule],
     exports: [InputComponent, RadioComponent, RatingComponent,
-              CommonModule, FormControlName, ReactiveFormsModule]
+        CommonModule, FormControlName, ReactiveFormsModule]
 })
 
-export class SharedModule{}
+export class SharedModule {
+    static forRoot(): ModuleWithProviders {
+        return {
+            ngModule: SharedModule,
+            providers: [ShoppingCartService, RestaurantsServices, OrderService]
+        }
+    }
+}
