455c59d4b3eb3bf9fb82957f076e5374dd3cd43c
Elton Andrade <eltonsa75@hotmail.com>
2019-05-03 00:49:45 -0300

    Disparando Login Manualmente e pegando URL

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import { OrderSumaryComponent } from './order-sumary/order-sumary.component';
 import { SharedModule } from './shared/shared.module';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { LoginComponent } from './security/login/login.component';
+import { UserDetailComponent } from './header/user-detail/user-detail.component';
 
 
 
@@ -38,7 +39,8 @@ import { LoginComponent } from './security/login/login.component';
     ReviewsComponent,
     OrderSumaryComponent,
     NotFoundComponent,
-    LoginComponent
+    LoginComponent,
+    UserDetailComponent
   ],
   imports: [
     BrowserModule,
